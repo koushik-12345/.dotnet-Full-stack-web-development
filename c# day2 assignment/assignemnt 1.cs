0ccecb11b0108c281506e760c8d4e6bd5c5bdb40
koushik-12345 <114811431+koushik-12345@users.noreply.github.com>
2024-09-03 18:15:02 +0530

    Add files via upload

---
+++ c# day2 assignment/assignemnt 1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int eid;
+            string ename;
+            string ejob;
+            int salary;
+
+            Console.WriteLine("enter your emplyee name:");
+            eid=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter your ename:");
+            ename = Console.ReadLine();
+
+            Console.WriteLine("enter your ejob:");
+            ejob = Console.ReadLine();
+
+            Console.WriteLine("enter your emplyee salary:");
+            salary = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("_____user details________");
+            Console.WriteLine("employee id:" + eid);
+            Console.WriteLine("employee name:"+ename);
+            Console.WriteLine("employee job:"+ejob);
+            Console.WriteLine("employee salary:" + salary);
+            Console.ReadLine();
+
+
+        }
+    }
+}
