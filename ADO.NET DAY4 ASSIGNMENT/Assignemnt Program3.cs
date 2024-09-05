7d76d1e57ccf39376f864ef7106018839c74656c
koushik-12345 <114811431+koushik-12345@users.noreply.github.com>
2024-09-05 16:36:25 +0530

    Add files via upload

---
+++ ADO.NET DAY4 ASSIGNMENT/Assignemnt Program3.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "Server=USHYDKKANAGALA1; Database=Employee; Integrated Security=true";
+
+            string cmdText = "SELECT * FROM PRODUCTS";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmdText, connStr);
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds);
+
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                Console.WriteLine("{0},{1},{2},{3},{4}", row["PId"].ToString(), row["Pname"].ToString(), row["Price"].ToString(), row["quantity"].ToString(), row["category"].ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ADO.NET DAY4 ASSIGNMENT/Assignment Program2.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient; 
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "Server=USHYDKKANAGALA1; Database=Employee; Integrated Security=true";
+
+            SqlConnection conn = new SqlConnection(connStr);
+            // conn.ConnectionString = connStr;
+
+            string cmdText = string.Format("INSERT INTO PRODUCTS VALUES({0},'{1}',{2},{3},'{4}')",120,"apple",1800,20,"mobile");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+
+            conn.Open();
+            Console.WriteLine("Connected to Database successfully");
+
+            int n = cmd.ExecuteNonQuery();  // For DML:  Insert, update, delete
+
+            Console.WriteLine("No. of rows affected :" + n);
+            conn.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
