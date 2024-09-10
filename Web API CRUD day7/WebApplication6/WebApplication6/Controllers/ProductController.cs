6af0a36ca1273b9e3cadd8497001cb4b0b59b38c
koushik-12345 <114811431+koushik-12345@users.noreply.github.com>
2024-09-10 16:35:03 +0530

    Add files via upload

---
+++ Web API CRUD day7/WebApplication6/WebApplication6/Controllers/ProductController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication16.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public SalesDbContext _context;
+
+        public ProductController(SalesDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            List<Product> Products = _context.Products.ToList();
+            return Ok(Products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+           Product obj = _context.Products.Find(id);
+
+            if (obj != null)
+            {
+                return Ok(obj);
+            }
+            else
+            {
+                return NotFound(new { status = "RequestedProducts details does not exists." });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product obj)
+        {
+            _context.Products.Add(obj);
+            _context.SaveChanges();
+            return Ok(new { status = "New Products details added in server." });
+        }
+
+        [HttpPut]
+        public IActionResult EditStudent(Product obj)
+        {
+            _context.Products.Update(obj);
+            _context.SaveChanges();
+            return Ok(new { status = "Product details updated in server." });
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudentById(int id)
+        {
+           Product obj = _context.Products.Find(id);
+
+            if (obj != null)
+            {
+                _context.Products.Remove(obj);
+                _context.SaveChanges();
+                return Ok(new { status = "Product details are deleted from server." });
+            }
+            else
+            {
+                return NotFound(new { status = "Requested product details does not exists." });
+            }
+        }
+
+
+
+    }
+}
