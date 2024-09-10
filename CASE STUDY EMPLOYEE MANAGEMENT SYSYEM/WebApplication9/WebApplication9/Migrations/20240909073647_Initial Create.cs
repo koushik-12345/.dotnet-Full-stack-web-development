45e275e1afa9aa924842eaeded56f73fddb7c433
koushik-12345 <114811431+koushik-12345@users.noreply.github.com>
2024-09-10 11:26:11 +0530

    Add files via upload

---
+++ CASE STUDY EMPLOYEE MANAGEMENT SYSYEM/WebApplication9/WebApplication9/Migrations/20240909073647_Initial Create.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication9.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Job = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    salary = table.Column<int>(type: "int", nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ CASE STUDY EMPLOYEE MANAGEMENT SYSYEM/WebApplication9/WebApplication9/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebApplication9.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int salary { get; set; }
+        public string Department { get; set; }
+
+    }
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<User> Users { get; set; }
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options) { }
+
+        protected EmployeeDbContext()
+        {
+        }
+    }
+}
