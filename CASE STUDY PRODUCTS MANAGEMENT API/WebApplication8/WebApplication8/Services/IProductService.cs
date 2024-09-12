07d10439a9409d0aeff7eeb49ecff5d44e35bf11
koushik-12345 <114811431+koushik-12345@users.noreply.github.com>
2024-09-12 12:26:46 +0530

    Add files via upload

---
+++ CASE STUDY PRODUCTS MANAGEMENT API/WebApplication8/WebApplication8/Services/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using WebApplication8.models.WebApplication8.models;
+
+namespace WebApplication8.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetAllProducts();
+        Product GetProductById(int id);
+        void AddProduct(Product productObj);
+        void UpdateProduct(Product productobj);
+
+        void DeleteProduct(Product productobj);
+        //void categoryProduct(Product produtobj);
+        List<Product> GetProductsByCategory(string category);
+        List<Product> GetOutOfStockProducts();
+        List<Product> GetProductsByPriceRange(decimal minPrice, decimal maxPrice);
+        List<string> GetAllCategories();
+    }
+}
